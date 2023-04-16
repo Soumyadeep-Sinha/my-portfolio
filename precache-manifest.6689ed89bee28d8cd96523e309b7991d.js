4a78a9feb91eece9d582556a1911f3adfd26eb9e
Soumyadeep-Sinha <soumyadeep8402@gmail.com>
2023-04-16 11:56:35 +0530

    Updates

---
+++ precache-manifest.6689ed89bee28d8cd96523e309b7991d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "177e660abd5c6fe97ea9",
+    "url": "/my-portfolio/static/css/main.ec76e822.chunk.css"
+  },
+  {
+    "revision": "177e660abd5c6fe97ea9",
+    "url": "/my-portfolio/static/js/main.177e660a.chunk.js"
+  },
+  {
+    "revision": "62e3fe9d2bfb51defa06",
+    "url": "/my-portfolio/static/js/1.62e3fe9d.chunk.js"
+  },
+  {
+    "revision": "f2bb3e8d5477afa4b7c6",
+    "url": "/my-portfolio/static/js/runtime~main.f2bb3e8d.js"
+  },
+  {
+    "revision": "89c2c0674bdba3d61daf6b1b1e5e82b5",
+    "url": "/my-portfolio/static/media/github.89c2c067.png"
+  },
+  {
+    "revision": "9ec3bce3b3800cc5d61563be9c9c8708",
+    "url": "/my-portfolio/static/media/instagram.9ec3bce3.png"
+  },
+  {
+    "revision": "f513be74baedd5c16bdd94ac43602c30",
+    "url": "/my-portfolio/static/media/linkedin.f513be74.svg"
+  },
+  {
+    "revision": "a113a3a94abfae3d298738a9b59731f4",
+    "url": "/my-portfolio/static/media/twitter.a113a3a9.png"
+  },
+  {
+    "revision": "b2a0261dfb8d297a73a0f989c544add9",
+    "url": "/my-portfolio/static/media/myavatar.b2a0261d.png"
+  },
+  {
+    "revision": "d3074281ff407bd0ad17b76986164d63",
+    "url": "/my-portfolio/static/media/CV.d3074281.pdf"
+  },
+  {
+    "revision": "1683e0b29312153ec03bd2ecec6bdbb0",
+    "url": "/my-portfolio/static/media/profile.1683e0b2.jpg"
+  },
+  {
+    "revision": "85644f7c36684dfe8fd48917795c7803",
+    "url": "/my-portfolio/static/media/certificate1.85644f7c.png"
+  },
+  {
+    "revision": "ea011b83211278a562136c09d01d784e",
+    "url": "/my-portfolio/static/media/certificate3.ea011b83.png"
+  },
+  {
+    "revision": "fc4b06efb8bb1c78040b16a6bd06fb5a",
+    "url": "/my-portfolio/static/media/certificate4.fc4b06ef.png"
+  },
+  {
+    "revision": "7d256837698529ad9bff222d45484070",
+    "url": "/my-portfolio/static/media/certificate6.7d256837.png"
+  },
+  {
+    "revision": "a4f9ff3f42ae376797be81a8c4768af0",
+    "url": "/my-portfolio/static/media/project1.a4f9ff3f.png"
+  },
+  {
+    "revision": "14bc8c240d9cf2420ab700b7458710f6",
+    "url": "/my-portfolio/static/media/project3.14bc8c24.png"
+  },
+  {
+    "revision": "d1b9f6a64168ecb127eae06592a69957",
+    "url": "/my-portfolio/index.html"
+  }
+];
